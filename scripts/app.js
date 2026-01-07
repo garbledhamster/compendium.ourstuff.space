d32e25989f5e3909ab729d33ec83c1fe24cf855e
GarbledHamster <jrice@ourstuff.space>
2026-01-07 10:48:43 -0600

    Show display names in entry bylines

--- scripts/app.js
+++ scripts/app.js
@@ -74,6 +74,7 @@ function initEditorTabs() {
   }
 
   let entries = null;
+  let compendiums = null;
   const settingsState = await initSettings({
     user,
     themes,
@@ -81,13 +82,17 @@ function initEditorTabs() {
     postNameInputEl: $("#postNameSetting"),
     postNameSaveEl: $("#postNameSave"),
     postNameHintEl: $("#postNameHint"),
-    onProfileChange: (nextName) => entries?.setPostName(nextName)
+    onProfileChange: (nextName) => {
+      entries?.setPostName(nextName);
+      compendiums?.setPostName(nextName);
+    }
   });
 
   entries = initEntries({ user, postName: settingsState.postName });
 
-  initCompendiums({
+  compendiums = initCompendiums({
     user,
+    postName: settingsState.postName,
     onSelectCompendium: (scope, compId, compDoc) => {
       entries.setActiveCompendium(scope, compId, compDoc);
     }
